b56c7470d43d1c8c2be41843aac4fef492fb6217
Antoine <antoine.berthelot@efrei.net>
2022-05-13 10:59:23 +0200

    modification Fonction règle alligner 3 chiffre saisie sécurisé

--- main.c
+++ main.c
@@ -25,7 +25,8 @@ void afficherMat(int** M, int size)
     {
         for (int j =0;j<size;j++)
         {
-            printf("%d\t ",M[i][j]);
+            if (M[i][j])
+            printf("\t ");
         }
         printf("\n");
     }
@@ -44,7 +45,6 @@ void RenplireMatrice4(int** Mat)
             Mat[i][j] = MatS[i][j];
         }
     }
-    //afficherMat(Mat[4][4],4);
 }
 
 void RenplireMatrice8(int** Mat)
@@ -96,27 +96,39 @@ void CreeMatJeux(int** Ms,int** Mc,int** Mj,int size)
     }
 }
 
-void regle3chiffre(int col, int lig, int** M, int val) {
+void regle3chiffre(int col, int lig, int** M, int val, int size) {
 
     //Pour quand y'en a deux d'affiler
-    if (M[lig+1][col] == val && M[lig+2][col] == val){
-        printf("On ne peux pas aligner plus de 2 %d", &val);
+    if (lig < size-2 ) {
+        if (M[lig+1][col] == val && M[lig+2][col] == val){
+            printf("On ne peux pas aligner plus de 2 %d", &val);
+        }
     }
-    if (M[lig-1][col] == val && M[lig-2][col] == val){
-        printf("On ne peux pas aligner plus de 2 %d", &val);
+    if (lig > 1) {
+        if (M[lig - 1][col] == val && M[lig - 2][col] == val) {
+            printf("On ne peux pas aligner plus de 2 %d", &val);
+        }
     }
-    if (M[lig][col+1] == val && M[lig][col+2] == val){
-        printf("On ne peux pas aligner plus de 2 %d", &val);
+    if (col < size-2) {
+        if (M[lig][col + 1] == val && M[lig][col + 2] == val) {
+            printf("On ne peux pas aligner plus de 2 %d", &val);
+        }
     }
-    if (M[lig][col-1] == val && M[lig][col-2] == val){
-        printf("On ne peux pas aligner plus de 2 %d", &val);
+    if (col > 1) {
+        if (M[lig][col-1] == val && M[lig][col-2] == val){
+            printf("On ne peux pas aligner plus de 2 %d", &val);
+        }
     }
     //Pour quand il essaye d'en mettre un au milieu
-    if (M[lig+1][col] == val && M[lig-1][col] == val){
-        printf("On ne peux pas aligner plus de 2 %d", &val);
+    if (lig > 0 && lig < size-1) {
+        if (M[lig + 1][col] == val && M[lig - 1][col] == val) {
+            printf("On ne peux pas aligner plus de 2 %d", &val);
+        }
     }
-    if (M[lig][col+1] == val && M[lig][col-1] == val){
-        printf("On ne peux pas aligner plus de 2 %d", &val);
+    if (col > 0 && col < size-1) {
+        if (M[lig][col + 1] == val && M[lig][col - 1] == val) {
+            printf("On ne peux pas aligner plus de 2 %d", &val);
+        }
     }
 }
 
@@ -133,7 +145,7 @@ void regleautant(int col, int lig, int** M, int val, int size) {
             }
         }
         if (compteur1 > size/2 || compteur2 > size/2){
-           printf("trop de %d dans la ligne ou la colone", &val);
+            printf("trop de %d dans la ligne ou la colone", &val);
         }
     }
     if (val == 0){
@@ -159,13 +171,13 @@ int main() {
     int vie=3;
 
     do {
-    printf("choisisez la taille de votre grille de jeux :(4 ou 8)");
+        printf("choisisez la taille de votre grille de jeux :(4 ou 8)");
         scanf("%d", &size);
     }while(size != 4 && size !=8);
 
     do {
-    printf("choisire votre difficulter:(1=facile, 2=moyen, 3=difficile)");
-    scanf("%d",&dif);
+        printf("choisire votre difficulter:(1=facile, 2=moyen, 3=difficile)");
+        scanf("%d",&dif);
     }while(dif != 1 && dif !=2 && dif !=3);
 
 
@@ -187,20 +199,20 @@ int main() {
     printf("Choisissez une case en indicant la colone puis la ligne\n");
 
     do {
-    printf("Colone :");
-    scanf("%d",&col );
+        printf("Colone :");
+        scanf("%d",&col );
     }while(col<1 || col>size);
     col = col -1;
 
     do {
-    printf("Ligne :");
-    scanf("%d",&lig );
+        printf("Ligne :");
+        scanf("%d",&lig );
     }while(lig<1 || lig>size);
     lig = lig -1;
 
     do {
-    printf("Souhaitez vous placer un 1 ou un 0 ?\n");
-    scanf("%d",&val );
+        printf("Souhaitez vous placer un 1 ou un 0 ?\n");
+        scanf("%d",&val );
     }while(val<0 || val>1);
 
     if (Msol[lig][col] == val) {
